ee4c8574aa48970602aa4a3923b526164535f2de
FerPR9 <ferp14406@gmail.com>
2024-11-05 16:28:32 -0600

    Card y paginas de catalogo de productos base

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,19 @@
-import { SplashScreen, Stack } from 'expo-router'
-import { useFonts } from 'expo-font'
-import { useEffect } from 'react'
+import { Stack } from 'expo-router';
+import { SplashScreen } from 'expo-router';
+//import { CartProvider } from './CartContext';
 
 export default function RootLayout() {
   return (
+    //<CartProvider>
     <Stack>
       <Stack.Screen
         name='index'
         options={{ headerShown: false }}
       />
+      <Stack.Screen name="product-list" options={{ headerShown: true, title: 'Products' }} />
+      <Stack.Screen name="product-detail" options={{ headerShown: true, title: 'Product Details' }} />
+      <Stack.Screen name="cart" options={{ headerShown: true, title: 'Cart' }} />
     </Stack>
-  )
+   // </CartProvider>
+  );
 }
